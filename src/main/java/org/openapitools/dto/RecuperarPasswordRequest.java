48c782c7fe86dcb3a9b0ba26afc951b8747857b2
Sebas2103 <juans.gonzalezm@uqvirtual.edu.co>
2025-04-07 10:38:27 -0500

    Schema Notificaciones

--- src/main/java/org/openapitools/dto/RecuperarPasswordRequest.java
+++ src/main/java/org/openapitools/dto/RecuperarPasswordRequest.java
@@ -6,6 +6,6 @@ import jakarta.validation.constraints.NotBlank;
 public record RecuperarPasswordRequest(
         @NotBlank(message = "El campo es requerido")
         @Email(message = "Debe ser un email válido")
-        String correo
+        String email
 ) {
 }
